3055ddc5b4cf6e1b658fb764428b83f0a9de0c8e
DanielEduardoCarrascoLainez <dcarrasco04@unitec.edu>
2023-03-24 17:21:58 -0600

    Primer commit

---
+++ src/examen2labp2_danielcarrasco/Torneo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen2labp2_danielcarrasco;
+
+import java.util.ArrayList;
+
+public class Torneo {
+    String nombreT;
+    ArrayList <Equipos> equiposParticipantes= new ArrayList();
+    ArrayList <Partidos> Partidos= new ArrayList();
+    
+    
+    /*
+    Torneo: Cuenta con un Nombre, una lista de Equipos participantes (la cual deberá ser
+organizada en orden descendente según los criterios descritos más a detalle por su
+instructor) y una lista de partidos.
+    */
+}
